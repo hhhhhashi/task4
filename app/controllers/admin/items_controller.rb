0f32e5e82721f3c2d6d277c63837fb0f6ea0c293
EC2 Default User <ec2-user@ip-172-31-9-205.ap-northeast-1.compute.internal>
2022-12-04 12:26:18 +0000

    [ECサイトのitem,showページの作成]70%

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -23,7 +23,7 @@ class Admin::ItemsController < ApplicationController
 
   def update
     item =Item.find(params[:id])
-    item.update
+    item.update(post_image_params)
     redirect_to admin_items_path
   end
 
