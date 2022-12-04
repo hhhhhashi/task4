cc78b6907593f6871a94212e17d8b4fa1736b982
EC2 Default User <ec2-user@ip-172-31-9-205.ap-northeast-1.compute.internal>
2022-12-04 05:57:19 +0000

    [customerのheader作成、login,sign_up画面の作成]

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -6,7 +6,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(resource)
-    
+    public_path
   end
 
   # GET /resource/sign_up
