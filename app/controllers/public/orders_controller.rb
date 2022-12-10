8d6d2fd8fcca558d57bd93d971d885ec49346a6c
EC2 Default User <ec2-user@ip-172-31-9-205.ap-northeast-1.compute.internal>
2022-12-10 14:09:15 +0000

    [注文情報入力画面]60%

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -14,6 +14,26 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    # :select_addressの中身に応じてcustomer? address? new?
+    if params[:selected_address] == 0
+      # current_customerからデータを挿入する
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:selected_address] == 1
+      # addressのテーブル
+      # :address_idという値でAddressテーブルのIDを送っているのでそれを使って探す(ex. Address.find(params[]))
+      address = Address.find(params[:address_id])
+      @order.postal_code = address.postal_code
+      @order.address = address.address
+      @order.name = address.name
+    else
+      # parameterから直接挿入
+      @order.postal_code = params[:postal_code]
+      @order.address = params[:address]
+      @order.name = params[:name]
+    end
     binding.pry
   end
 
@@ -22,7 +42,7 @@ class Public::OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:payment_method)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :customer
 
+  def display_address
+    "〒" + postal_code + " " + address + " " + name
+  end
 end
