a334d21a40c7662623d0b2cb40c1db25e48f59d6
EC2 Default User <ec2-user@ip-172-31-9-205.ap-northeast-1.compute.internal>
2022-12-19 12:50:46 +0000

    [admin注文詳細画面の製作ステータス、レイアウト作成]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,9 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order=Order.find(params[:id])
+    @order_details=@order.order_details
+    @total_price=0
+
   end
 
   def update
@@ -11,6 +14,9 @@ class Admin::OrdersController < ApplicationController
     #binding.pry
   end
 end
+
+
+
 
 private
   def order_params
